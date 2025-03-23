0ef30990e6d0803d3b7fd65a8900bc7aaa3ac00e
Ildarc4S <ildarc4S@gmail.com>
2025-03-23 23:51:41 +0300

    Refactof function len

--- brick_game/tetris/tetramino_collection.c
+++ brick_game/tetris/tetramino_collection.c
@@ -4,44 +4,37 @@
 
 void initAllTetraminoCollectionBricks(TetraminoCollection_t *self) {
   Tetramino_t tetramino_i = {
-      .x = 0,
-      .y = 0,
+      .x = 0, .y = 0,
       .color = TETRAMINO_COLOR_RED,
       .brick = {{0, 0, 0, 0}, {1, 1, 1, 1}, {0, 0, 0, 0}, {0, 0, 0, 0}}};
 
   Tetramino_t tetramino_o = {
-      .x = 0,
-      .y = 0,
+      .x = 0, .y = 0,
       .color = (Color_t)OBJECT_CODE_TETRAMINO_O,
       .brick = {{0, 0, 0, 0}, {0, 1, 1, 0}, {0, 1, 1, 0}, {0, 0, 0, 0}}};
 
   Tetramino_t tetramino_t = {
-      .x = 0,
-      .y = 0,
+      .x = 0, .y = 0,
       .color = TETRAMINO_COLOR_BLUE,
       .brick = {{0, 0, 0, 0}, {0, 1, 0, 0}, {1, 1, 1, 0}, {0, 0, 0, 0}}};
 
   Tetramino_t tetramino_s = {
-      .x = 0,
-      .y = 0,
+      .x = 0, .y = 0,
       .color = TETRAMINO_COLOR_GREEN,
       .brick = {{0, 0, 0, 0}, {0, 1, 1, 0}, {1, 1, 0, 0}, {0, 0, 0, 0}}};
 
   Tetramino_t tetramino_z = {
-      .x = 0,
-      .y = 0,
+      .x = 0, .y = 0,
       .color = TETRAMINO_COLOR_DARK_MAGNETA,
       .brick = {{0, 0, 0, 0}, {1, 1, 0, 0}, {0, 1, 1, 0}, {0, 0, 0, 0}}};
 
   Tetramino_t tetramino_j = {
-      .x = 0,
-      .y = 0,
+      .x = 0, .y = 0,
       .color = TETRAMINO_COLOR_DARK_YELLOW,
       .brick = {{0, 0, 0, 0}, {1, 0, 0, 0}, {1, 1, 1, 0}, {0, 0, 0, 0}}};
 
   Tetramino_t tetramino_l = {
-      .x = 0,
-      .y = 0,
+      .x = 0, .y = 0,
       .color = TETRAMINO_COLOR_YELLOW,
       .brick = {{0, 0, 0, 0}, {0, 0, 1, 0}, {1, 1, 1, 0}, {0, 0, 0, 0}}};
 

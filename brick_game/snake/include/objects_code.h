47582fca1ad9f1b95768678a69403f8a6cfd6b3f
Ildarc4S <ildarc4S@gmail.com>
2025-03-26 00:08:02 +0300

    Add objects enums file

---
+++ brick_game/snake/include/objects_code.h
@@ -0,0 +1,41 @@
+#ifndef OBJECTS_CODE_H
+#define OBJECTS_CODE_H
+
+namespace s21 {
+
+enum class State {
+  kStart = 0,
+  kSpawn,
+  kMove,
+  kEat,
+  kPause,
+  kGameOver,
+  kExit
+};
+
+enum class Direction {
+  kUp,
+  kDown,
+  kLeft,
+  kRight
+};
+
+enum class PauseMode {
+  kPause = 1,
+  kGameContinue = 2,
+  kStart = 3,
+  kGameOver = 4,
+  kExit = 5,
+  kWin = 6
+};
+
+enum class FigureCode {
+  kAir = -2,
+  kWall = -1,
+  kSnake = 10,
+  kApple = 11
+};
+
+}  // namespace s21
+
+#endif  // OBJECTS_CODE_H
